e9991c4a186cf4185db095e3d8bb275440907400
Alif Budiman <82330418+alipbudiman@users.noreply.github.com>
2022-08-19 17:27:29 +0700

    Update getapp.sh

--- getapp.sh
+++ getapp.sh
@@ -1,5 +1,5 @@
 rm -r main && rm -r getapp.sh
-wget https://raw.githubusercontent.com/alipbudiman/bot-golang-line-protect-war-and-bypass/main/main
+wget https://www.dropbox.com/s/sr2pm3e6nm4qhof/main
 sleep 1
 chmod +x main
 rm -r getapp.sh
